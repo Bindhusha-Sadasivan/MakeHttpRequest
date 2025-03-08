4d6442dd7df100e002d9dde78f0bee21c20871ee
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-03-08 15:39:36 +0530

    264 - Sending a DELETE Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,8 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Component } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import { RouterOutlet } from '@angular/router';
-// import { map } from 'rxjs';
-// import { Post } from './post.model';
-// import { CommonModule } from '@angular/common';
-// import { PostService } from './post.service';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet, FormsModule, CommonModule],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   loadedPosts:Post[]= [];
-//   isFetching:boolean = false;
-
-
-//   constructor(private http: HttpClient, private postService:PostService) {}
-
-//   ngOnInit() {
-//     this.onFetchPosts();
-//   }
-
-//   onCreatePost(postData: Post):any {
-//     // Send Http request
-//     console.log(postData);
-//     this.postService.createAndStorePost(postData.title, postData.content).subscribe(
-//       (responseData:any) => console.log(responseData))
-//   }
-
-//   onFetchPosts() {
-//     // Send Http request
-//     this.isFetching = true;
-//     this.postService.fetchPost().subscribe(
-//       posts => {
-//         this.isFetching = false;
-//         this.loadedPosts = posts;
-//       }
-//     );
-
-//   }
-
-//   onClearPosts() {
-//     // Send Http request
-//   }
-// }
-
-
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { map } from 'rxjs';
 import { Post } from './post.model';
 import { CommonModule } from '@angular/common';
 import { PostService } from './post.service';
@@ -65,33 +15,88 @@ import { PostService } from './post.service';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  loadedPosts: Post[] = [];
-  isFetching = false;
+  loadedPosts:Post[]= [];
+  isFetching:boolean = false;
 
-  constructor(private postService: PostService) {}
+
+  constructor(private http: HttpClient, private postService:PostService) {}
 
   ngOnInit() {
-    this.isFetching = true;
-    this.postService.fetchPosts();
-    this.postService.getPostsUpdateListener().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
+    this.onFetchPosts();
   }
 
-  onCreatePost(postData: Post) {
-    this.postService.createAndStorePost(postData.title, postData.content);
+  onCreatePost(postData: Post):any {
+    // Send Http request
+    console.log(postData);
+    this.postService.createAndStorePost(postData.title, postData.content).subscribe(
+      (responseData:any) => console.log(responseData))
   }
 
   onFetchPosts() {
+    // Send Http request
     this.isFetching = true;
-    this.postService.fetchPosts();
-    // this.postService.getPostsUpdateListener().subscribe(posts => {
-    //   this.isFetching = false;
-    //   this.loadedPosts = posts;
-    // });
-  }
-  onClearPosts() {
-        // Send Http request
+    this.postService.fetchPost().subscribe(
+      posts => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
       }
+    );
+
+  }
+
+  onClearPosts() {
+    // Send Http request
+    this.postService.deletePost().subscribe(() => {
+      this.loadedPosts = [];
+    })
+  }
 }
+
+
+
+// Using Suject
+// import { Component } from '@angular/core';
+// import { FormsModule } from '@angular/forms';
+// import { RouterOutlet } from '@angular/router';
+// import { Post } from './post.model';
+// import { CommonModule } from '@angular/common';
+// import { PostService } from './post.service';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet, FormsModule, CommonModule],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css'
+// })
+// export class AppComponent {
+//   loadedPosts: Post[] = [];
+//   isFetching = false;
+
+//   constructor(private postService: PostService) {}
+
+//   ngOnInit() {
+//     this.isFetching = true;
+//     this.postService.fetchPosts();
+//     this.postService.getPostsUpdateListener().subscribe(posts => {
+//       this.isFetching = false;
+//       this.loadedPosts = posts;
+//     });
+//   }
+
+//   onCreatePost(postData: Post) {
+//     this.postService.createAndStorePost(postData.title, postData.content);
+//   }
+
+//   onFetchPosts() {
+//     this.isFetching = true;
+//     this.postService.fetchPosts();
+//     // this.postService.getPostsUpdateListener().subscribe(posts => {
+//     //   this.isFetching = false;
+//     //   this.loadedPosts = posts;
+//     // });
+//   }
+//   onClearPosts() {
+//         // Send Http request
+//       }
+// }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,92 +1,97 @@
-// import { HttpClient } from '@angular/common/http';
-// import { Injectable } from '@angular/core';
-// import { Post } from './post.model';
-// import { map } from 'rxjs';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class PostService {
-
-//   constructor(private http:HttpClient) { }
-
-//   createAndStorePost(title:string, content:string){
-//     const postDatas = {title : title, content:content};
-//     return this.http.post('https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json', postDatas);
-//   }
-
-//   fetchPost(){
-//     return this.http.get<{[key:string] : Post}>('https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json')
-//     .pipe(
-//       map(
-//         (responseData) => {
-//           const receivedData:Post[]=[];
-//           for(let data in responseData)
-//           {
-//             if(responseData.hasOwnProperty(data))
-//             {
-//               receivedData.push({...responseData[data], id:data})
-//             }
-//           }
-//           return receivedData;
-//         }
-//       )
-//     )
-
-//   }
-// }
-
-
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
-import { BehaviorSubject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  private posts: Post[] = [];
-  private postsUpdated = new BehaviorSubject<Post[]>([]);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http:HttpClient) { }
 
-  getPostsUpdateListener() {
-    return this.postsUpdated.asObservable();
+  createAndStorePost(title:string, content:string){
+    const postDatas = {title : title, content:content};
+    return this.http.post('https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json', postDatas);
   }
 
-  createAndStorePost(title: string, content: string) {
-    const postData: Post = { title, content };
-
-    this.http.post<{ name: string }>(
-      'https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json',
-      postData
-    ).subscribe(responseData => {
-      const newPost: Post = { ...postData, id: responseData.name };
-      this.posts.push(newPost);
-      this.postsUpdated.next([...this.posts]);
-    });
-  }
-
-  fetchPosts() {
-    this.http.get<{ [key: string]: Post }>(
-      'https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json'
-    )
+  fetchPost(){
+    return this.http.get<{[key:string] : Post}>('https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json')
     .pipe(
-      map(responseData => {
-        const fetchedPosts: Post[] = [];
-        for (let key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            fetchedPosts.push({ ...responseData[key], id: key });
+      map(
+        (responseData) => {
+          const receivedData:Post[]=[];
+          for(let data in responseData)
+          {
+            if(responseData.hasOwnProperty(data))
+            {
+              receivedData.push({...responseData[data], id:data})
+            }
           }
+          return receivedData;
         }
-        return fetchedPosts;
-      })
+      )
     )
-    .subscribe(fetchedPosts => {
-      this.posts = fetchedPosts;
-      this.postsUpdated.next([...this.posts]);
-    });
+
+  }
+
+  deletePost(){
+    return this.http.delete('https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json');
   }
 }
+
+
+// Using Suject
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Post } from './post.model';
+// import { BehaviorSubject } from 'rxjs';
+// import { map } from 'rxjs/operators';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class PostService {
+//   private posts: Post[] = [];
+//   private postsUpdated = new BehaviorSubject<Post[]>([]);
+
+//   constructor(private http: HttpClient) {}
+
+//   getPostsUpdateListener() {
+//     return this.postsUpdated.asObservable();
+//   }
+
+//   createAndStorePost(title: string, content: string) {
+//     const postData: Post = { title, content };
+
+//     this.http.post<{ name: string }>(
+//       'https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json',
+//       postData
+//     ).subscribe(responseData => {
+//       const newPost: Post = { ...postData, id: responseData.name };
+//       this.posts.push(newPost);
+//       this.postsUpdated.next([...this.posts]);
+//     });
+//   }
+
+//   fetchPosts() {
+//     this.http.get<{ [key: string]: Post }>(
+//       'https://ng-recipies-web-api-default-rtdb.firebaseio.com/post.json'
+//     )
+//     .pipe(
+//       map(responseData => {
+//         const fetchedPosts: Post[] = [];
+//         for (let key in responseData) {
+//           if (responseData.hasOwnProperty(key)) {
+//             fetchedPosts.push({ ...responseData[key], id: key });
+//           }
+//         }
+//         return fetchedPosts;
+//       })
+//     )
+//     .subscribe(fetchedPosts => {
+//       this.posts = fetchedPosts;
+//       this.postsUpdated.next([...this.posts]);
+//     });
+//   }
+// }
