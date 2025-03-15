58e0893f57a78cb72afc302683236ee033bd5950
Bindhusha Sadasivan <bindhushasadasivan@gmail.com>
2025-03-15 15:49:35 +0530

    275 - Response Interceptors

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,9 +1,17 @@
-import { HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
+import { HttpEventType, HttpHeaders, HttpInterceptorFn } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  console.log("request is sending")
+  console.log("request is sending");
+  console.log("Request Url:" + req.url);
   const modifiedUrl = req.clone({headers: new HttpHeaders({
     "content-type" : "Recipies Request"
   })})
-  return next(modifiedUrl);
+  return next(modifiedUrl).pipe(tap((event:any) => {
+    console.log(event);
+    if(event.type === HttpEventType.Response){
+      console.log("Response Arrived, Body Data:");
+      console.log(event.body)
+    }
+  }));
 };
